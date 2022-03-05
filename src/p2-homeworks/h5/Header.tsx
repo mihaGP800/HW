e3b3135866880594ceebeb351abf6078ebcd53b3
mihaGP800 <mihagoroh@gmail.com>
2022-03-05 21:13:23 +0300

    WH5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -7,7 +7,7 @@ import {PATH} from "./Router";
 function Header() {
     return (
         <div>
-            <NavLink to={PATH.PRE_JUNIOR}>pre-junior </NavLink>
+            <NavLink to={PATH.PRE_JUNIOR} >pre-junior </NavLink>
             <NavLink to={PATH.JUNIOR}>junior </NavLink>
             <NavLink to={PATH.JUNIOR_PLUS}>junior-plus</NavLink>
 
