8c97e3eedfc1a465b9791a60924dd3e4740c61f4
mihaGP800 <mihagoroh@gmail.com>
2022-03-05 23:30:00 +0300

    HW5.1

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -4,14 +4,16 @@ import {PATH} from "./Router";
 import s from './Header.module.css'
 
 
-
 function Header() {
     return (
-        <div>
-            <NavLink to={PATH.PRE_JUNIOR} className={s.action}>pre-junior </NavLink>
-            <NavLink to={PATH.JUNIOR}>junior </NavLink>
-            <NavLink to={PATH.JUNIOR_PLUS}>junior-plus</NavLink>
-
+        <div className={s.header}>
+            <NavLink to={PATH.PRE_JUNIOR} className={navClick => navClick.isActive ? s.active
+                : `${s.active} ${s.passive}`}>pre-junior </NavLink>
+            <NavLink to={PATH.JUNIOR}
+                     className={navClick => navClick.isActive ? s.active : `${s.active} ${s.passive}`}>junior </NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}
+                     className={navClick => navClick.isActive ? s.active : `${s.active} ${s.passive}`}>junior-plus</NavLink>
+            <div className={s.text}>наведи</div>
 
 
         </div>
